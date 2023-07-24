97794918688ea9a7c1984f3b4432415ddc533ee4
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-07-24 04:16:29 -0300

    add cors

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 require('dotenv').config()
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
 const connectDB = require('./config/db')
 
-var app = express();
+const app = express();
 
 connectDB()
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 /* RUTAS */
 app.use('/', indexRouter);
